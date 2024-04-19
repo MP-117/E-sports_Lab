61d90d94ec1b5c6f43b15b8178c6f6ad6a194cdb
MP-117 <70386482+MP-117@users.noreply.github.com>
2024-04-19 13:40:00 -0500

    uploading my "fresh rebuild" folder
    Missing the Valorant and EpicInstallers since they are too large

---
+++ fresh rebuild/installEPIC.bat
@@ -0,0 +1,4 @@
+::installEPIC.bat written by Matthew Piotrowski 2024
+::this uses the EpicInstaller in the same directory and quietly installs it
+
+msiexec /i EpicInstaller-15.17.1.msi /quiet
---
+++ fresh rebuild/move shortcuts.bat
@@ -0,0 +1,9 @@
+::move shortcuts.bat written by Matthew Piotrowski 2024
+::moves game shortcuts from admin account desktop to public desktop,
+::which allows for users to get the shortcuts on their desktops
+
+move "C:\Users\E-sports\Desktop\Apex Legends.url" "C:\Users\Public\Desktop"
+move "C:\Users\E-sports\Desktop\Counter-Strike 2.url" "C:\Users\Public\Desktop"
+move "C:\Users\E-sports\Desktop\Tom Clancy's Rainbow Six Siege.url" "C:\Users\Public\Desktop"
+move "C:\Users\E-sports\Desktop\Rocket League*" "C:\Users\Public\Desktop"
+timeout /t -1
