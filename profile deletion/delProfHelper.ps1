aec1791745889d34269376432fd91191f8a5b732
MP-117 <70386482+MP-117@users.noreply.github.com>
2025-02-25 20:30:07 -0600

    finished delete profile command
    wrote a ps1 file that utilizes subinacl to allow delprof2 to fully and cleanly delete profiles

---
+++ profile deletion/delProfHelper.ps1
@@ -0,0 +1,17 @@
+﻿#delProfHelper written by Matthew Piotrowski 2025
+#gives administator profiles full access to local profiles' subkeys under UninstalledStoreApps
+
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows Search\UninstalledStoreApps\"
+
+#grabs Child subkeys and sends into pipeline and then a for loop for each object 
+Get-ChildItem -path $regPath -Include *-1786437548-* -Recurse -Name| ForEach-Object{
+    $localPath = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Search\UninstalledStoreApps\" + $_
+    #runs subinacl.exe
+    Invoke-Expression -Command ('.\subinacl.exe /noverbose /keyreg $localPath /grant=builtin\administrators=F')
+}
+
+#                         !WARNING!
+#This file is modfying admin access for registry keys.
+#This is intended for the subkeys under UninstalledStoreApps that contains "-1786437548-".
+#That value could always change, but it encompasses all local profiles from SIUE's "CAMPUS" domain at the time of this file's creation.
+#Please contact Matthew Piotrowski if any help is needed modifying this file for your intended use-case.
--- profile deletion/delete profile.bat
+++ profile deletion/delete profile.bat
@@ -1,7 +1,15 @@
-::delete-profile.bat written by Matthew Piotrowski 2024
+::delete-profile.bat written by Matthew Piotrowski 2024-2025
 ::used to clear all student profiles off of the Esports computers
-::Utilizes DelProf2 written by Helge Klein
+::Utilizes DelProf2 written by Helge Klein, as well as "subinacl.exe"
 
+::puts execution in current directory
 cd "%~dp0"
+
+::allows for delprof2 to delete a certain subkey that would be otherwise blocked
+Powershell.exe -ExecutionPolicy Bypass -File .\delProfHelper.ps1
+
+::runs DelProf2
 delprof2.exe /ed:E-sports /ed:Public /ed:Default /u
+
+::this is here for admins running this to observe the changes done
 timeout /t -1
